51894e660806a0d87ebd548b03cc8935874352d1
lucaslautier <lucas.lautier.20@um.edu.mt>
2022-01-05 14:22:27 +0100

    small update

--- crypto.java
+++ crypto.java
@@ -1,5 +1,7 @@
 public class crypto {
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
 }
+
---
+++ userAccount.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class userAccount {
+    
+    public userAccount() {
+        try{
+            Scanner s = new Scanner(System.in);
+            System.out.println("1.Create account");
+            System.out.println("2.Login");
+            System.out.print("Enter choice");
+            String choice = s.nextLine();
+
+            if (choice.equals("1")){
+
+            }
+        
+        }
+    }
+}

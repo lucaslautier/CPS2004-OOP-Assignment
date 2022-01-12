d92e6a660294034bbb9f51fc739a23e7a8a6f982
lucaslautier <lucas.lautier.20@um.edu.mt>
2022-01-12 22:04:50 +0100

    added funct to give binary of num entered as str

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -4,8 +4,10 @@
 
 int main(){
 
+    //INVERT
+    //myuint<4>M(2);      //<4> - No of bits || <5> Decimal to be converted
 
-    myuint<4>M(1);      //<4> - No of bits || <5> Decimal to be converted
-
-    myuint<4>M("8921");
+    myuint<4>M("2"); 
+    
+    //?Checker to see if number given is too big for bits?
 }
--- Task3/main.h
+++ Task3/main.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <iostream>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
 template <int T>
 
 class myuint{
     public:
-    int size = T;
+    int sizeBits = T;
     std::vector<bool> bin;
 
     //Function to make an integer(decimal) -> binary
     myuint(int num){
-        this->size = size;
+        this->sizeBits = sizeBits;
         int i = 0;
 
         //calculates if current bit gives 0 or 1, inputs into vec
@@ -21,14 +23,86 @@ class myuint{
             i++;
         }
         //fills remaining bits with 0s(weren't filled as decimal was already found)
-        for(i;i<size;i++){
+        for(i;i<sizeBits;i++){
             bin.push_back(0);
         }
 
+        reverse(bin.begin(), bin.end());
         //test output
         for(bool j:bin)
             std::cout << j;
     }
 
+    string divideString(string num){
+        //code for that  here
+        int divisor = 2;
+        string ans;
+
+        int idx = 0;
+        int temp = num[idx] - '0';
+
+        while(temp < divisor)
+        {
+            temp = temp*10 + (num[++idx] - '0');
+        }
+        while(num.size() > idx){
+            ans += (temp/divisor) + '0';
+            temp = (temp%divisor) *10 + num[++idx] -'0';
+        }
+
+        if(ans.length()==0){
+            return "0";
+        }
+
+        return ans;
+
+    }
+
+    //switch current digit to ASCII, check if it is odd or even
+    bool oddOrEven(int currNo){
+        
+        if(currNo % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     myuint(string s)
+    {
+     //code for that here
+        this->sizeBits = sizeBits;  
+        bool isEven = false;
+        int size = s.size();  
+        int i = 0;
+        while(s != "0"){
+            size = s.size();              //size of string
+            
+            int tmp = s[size-1] -'0';         //read last element in string
+            isEven = oddOrEven(tmp);          //check if odd or even
+        
+            if(isEven){
+                bin.push_back(0);             //if even-push 0
+                i++;
+            }else{
+                bin.push_back(1);             //if odd - push 1
+                i++;
+            }
+
+            s = divideString(s);
+
+        }
+
+        for(i;i<sizeBits;i++){
+            bin.push_back(0);
+        }
+        reverse(bin.begin(), bin.end());
+
+        for(bool j:bin)
+            std::cout << j;
+        //check if odd or even
+        
+    }
+
+
 };

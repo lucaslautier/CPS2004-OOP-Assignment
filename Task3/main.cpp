bd2f438469bcb839e31646def4e4c461b112903a
lucaslautier <lucas.lautier.20@um.edu.mt>
2022-01-16 17:34:27 +0100

    added shift left + right, started mult overloading

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -7,13 +7,30 @@
 int main(){
 
     //INVERT
-    myuint<4>M(1 );      //<4> - No of bits || <5> Decimal to be converted
+    cout << "Normal Output: " << endl;
+    myuint<4>OI(1 );      //<4> - No of bits || <5> Decimal to be converted
     myuint<4>T(5 );
+    myuint<4>OS("2"); 
+    cout << endl;
 
-    myuint<4>A  = M + T;
+    cout << "Addition: " << endl;
+    myuint<4>A  = OI + T;
+    cout << endl;
 
-    // cout << (A);
-    //myuint<4>M("2"); 
+    cout << "Shift Left: " << endl;
+    myuint<4>L = (6);
+    myuint<4>SL = L << 2;    //shift to the left by 1
+    cout << endl;
+    
+    cout << "Shift Right: " << endl;
+    myuint<4>R = (12);
+    myuint<4>SR = R >> 2;
+    
+    cout << "Multiply: " << endl;
+    myuint<4>M1(4);
+    myuint<4>M2(3);
+    //myuint<8>M = M1 * M2;
     
     //?Checker to see if number given is too big for bits?
 }
+
--- Task3/main.h
+++ Task3/main.h
@@ -107,15 +107,17 @@ class myuint{
         for(bool j:bin)
             std::cout << j;
         //check if odd or even
-        
+        cout << endl;
     }
 
     myuint operator + (const myuint& num)
     {
+        this->sizeBits = sizeBits;
         myuint<T> LHS = *this;
         myuint<T> tmp ;
         int carry = 0;
-        for(int i =bin.size() -1; i >= 0; --i){
+        //bin.size()
+        for(int i =sizeBits -1; i >= 0; --i){
             LHS.bin[i];
             num.bin[i];
 
@@ -140,13 +142,17 @@ class myuint{
 
             
         }
+        reverse(tmp.bin.begin(), tmp.bin.end());
+        for(bool j:tmp.bin)
+            std::cout << j;
 
-        int j = 0;
+        // int j = 0;
         
-        for(j = tmp.bin.size()-1; j >= 0 ; --j){
-            cout << tmp.bin[j];
-            // cout << "test" ;
-        }
+        // for(j = tmp.bin.size()-1; j >= 0 ; --j){
+        //     cout << tmp.bin[j];
+        //     // cout << "test" ;
+        // }
+        cout << endl;
             
         // cout << j;
 
@@ -155,5 +161,100 @@ class myuint{
     }
 
     
+    myuint operator << (int num){
+        myuint<T> LHS = *this;
+        myuint<T> tmp ;
+        this->sizeBits = sizeBits;
+        
+       
+        for(int j = 0; j < num; j++){
+            tmp.bin.push_back(0);
+        }
+
+        for(int i = sizeBits -1; i >= 0 ; --i){
+            tmp.bin.push_back(LHS.bin[i]);
+        }
+
+        //sanity check - shifted left before removing extra bits
+        // for(bool j:tmp.bin){
+        //     std::cout << j;
+        // }
+        // cout << endl;
+        
+        for(int x = tmp.bin.size(); x > sizeBits; --x){
+            tmp.bin.pop_back();
+        }
+
+        reverse(tmp.bin.begin(), tmp.bin.end());
+
+        for(bool j:tmp.bin){
+            std::cout << j;
+        }
+
+        cout << endl;
+
+        return tmp;
+    }
+
+    myuint operator >> (int num){
+        myuint<T> LHS = *this;
+        myuint<T> tmp ;
+        this->sizeBits = sizeBits;
+
+        for(int j = 0; j < num; j++){
+            tmp.bin.push_back(0);
+        }
+
+        for(int i = 0; i < sizeBits ; i++){
+            tmp.bin.push_back(LHS.bin[i]);
+        }
+
+        //sanity check - shifted left before removing extra bits
+        // for(bool j:tmp.bin){
+        //     std::cout << j;
+        // }
+        // cout<<endl;
+
+        for(int x = tmp.bin.size(); x > sizeBits; --x){
+            tmp.bin.pop_back();
+        }
+
+        for(bool j:tmp.bin){
+            std::cout << j;
+        }
+
+        cout << endl;
+
+        return tmp;
+    }
+
+    myuint operator * (const myuint& num){
+        this->sizeBits = sizeBits;
+        myuint<T> LHS = *this;
+        myuint<T> tmp;
+        int cntA = 0;
+        int cntB = 0;
+
+        /
+        for(int i = 0; i < LHS.bin.size(); i++){
+            if(LHS.bin[i] == 1){
+                cntA += 1;
+            }
+        }
+
+        for(int i = 0; i < num.bin.size(); i++){
+            if(num.bin[i] == 1){
+                cntB += 1;
+            }
+        }
+
+        //if one of them 
+        if(cntA > 0 && cntB > 0){
+
+        }
+
+
+    }
+
 
 };
